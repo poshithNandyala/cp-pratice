4f17b17b8643715961f69232143c5ab1d60463da
poshithNandyala <poshithiiitl@gmail.com>
2025-01-11 23:40:04 +0530

    Add solution for problem B_Bad_Boy in TLE 900

---
+++ anonymous/TLE/900/B_Bad_Boy.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    fastio int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+}
