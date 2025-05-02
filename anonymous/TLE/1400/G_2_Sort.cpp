57c6aba21288d5d3f9c2d677ec7a01db44a582f2
poshithNandyala <poshithiiitl@gmail.com>
2025-05-02 18:51:14 +0530

    Refactor code formatting for G_2_Sort.cpp; improve readability and consistency

--- anonymous/TLE/1400/G_2_Sort.cpp
+++ anonymous/TLE/1400/G_2_Sort.cpp
@@ -3,37 +3,40 @@ using namespace std;
 
 #define ll long long
 #define endl '\n'
-#define fastio \
+#define fastio                        \
     ios_base::sync_with_stdio(false); \
     cin.tie(nullptr);
 
 int main()
 {
-    fastio
-    int t;
+    fastio int t;
     cin >> t;
     while (t--)
     {
-        ll n,k;
-        cin>>n>>k;
-        vector<ll> a(n),b(n);
-        b[0]=0;
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-            if(i>=1 && a[i-1]>=2*a[i]){
-                b[i]=1; 
-            }        
+        ll n, k;
+        cin >> n >> k;
+        vector<ll> a(n), b(n);
+        b[0] = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            if (i >= 1 && a[i - 1] >= 2 * a[i])
+            {
+                b[i] = 1;
+            }
         }
-        for(int i=0;i<n;i++){
-            b[i]+=b[i-1];
+        for (int i = 0; i < n; i++)
+        {
+            b[i] += b[i - 1];
         }
-        ll ans=0;
-        for(int i=0;i<n-k;i++){
-              if(!(b[i+k]-b[i])){
+        ll ans = 0;
+        for (int i = 0; i < n - k; i++)
+        {
+            if (!(b[i + k] - b[i]))
+            {
                 ans++;
-              }
+            }
         }
-        cout<<ans<<endl;
-
+        cout << ans << endl;
     }
 }
