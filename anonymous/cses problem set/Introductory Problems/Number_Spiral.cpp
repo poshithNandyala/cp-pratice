83f91521efd4d55b4a219ef940bd0228404b1bb5
Poshith Nandyala <poshithiiitl@gmail.com>
2025-12-18 14:01:06 +0530

    feat: update Number Spiral solution and add corresponding image

--- anonymous/cses problem set/Introductory Problems/Number_Spiral.cpp
+++ anonymous/cses problem set/Introductory Problems/Number_Spiral.cpp
@@ -1,58 +1,32 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// #define int long long
-// #define endl '\n'
-// #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define int long long
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
-// void make_spiral(int n, vector<vector<int>>& a) {
-//     int turn = 2;
-//     int i = 1, j = 0;
-//     int v = 1;
-//     a[0][0] = v++;
-//     a[1][0] = v++;
-//     a[1][1] = v++;
-//     a[0][1] = v++;
-//     while (i < n && j < n) {
-//         if (j == 0) {
-//             i++;
-//             if (i >= n) break;
-//             while (j <= turn) {
-//                 a[i][j] = v++;
-//                 j++;
-//             }
-//             while (i >= 0) {
-//                 a[i][j] = v++;
-//                 i--;
-//             }
-//             turn++;
-//         }
-//         if (i == 0) {
-//             j++;
-//             if (j >= n) break;
-//             while (i <= turn) {
-//                 a[i][j] = v++;
-//                 i++;
-//             }
-//             while (j >= 0) {
-//                 a[i][j] = v++;
-//                 j--;
-//             }
-//             turn++;
-//         }
-//     }
-// }
-
-// int32_t main() {
-//     fastio
-//         int n = 5;
-//     vector<vector<int>> a(n, vector<int>(n, 0));
-//     make_spiral(n, a);
-//     int t;
-//     cin >> t;
-//     while (t--) {
-//         int x, y;
-//         cin >> x >> y;
-//         cout << a[x - 1][y - 1] << endl;
-//     }
-// }
+int32_t main() {
+    fastio
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        if (y >= x) {
+            if (y % 2) {
+                cout << y * y - x + 1 << endl;
+            }
+            else {
+                cout << (y-1)*(y-1) + x << endl;
+            }
+        }
+        else {
+            if (x % 2) {
+                cout << (x-1)*(x-1) + y << endl;
+            }
+            else {
+                cout << x * x - y + 1 << endl;
+            }
+        }
+    }
+}
