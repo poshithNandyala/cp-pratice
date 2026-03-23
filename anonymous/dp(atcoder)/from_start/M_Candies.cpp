ce6915a25ca3394e0e24cbb844e48214225a6e83
Poshith Nandyala <poshithiiitl@gmail.com>
2026-03-23 15:40:33 +0530

    added candies

---
+++ anonymous/dp(atcoder)/from_start/M_Candies.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+int rec(int i, int k) {
+
+
+    for(int i=1;)
+}
+
+
+int32_t main() {
+    fastio
+        int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0;i < n;i++) {
+        cin >> a[i];
+    }
+
+    rec(0, k);
+}
